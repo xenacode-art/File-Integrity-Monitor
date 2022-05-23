ec6e9861fe3a843f473cc8d6993d969ffe951252
xenacode-art <kaja.obinna@lmu.edu.ng>
2022-05-23 19:14:34 +0100

    added a function to convert small alph to capital

--- Untitled2.ps1
+++ Untitled2.ps1
@@ -32,3 +32,20 @@ Function  Calculate-File-Hash($filepath) {
  # Calc Hash from target files and store in baseline.txt
  # collect all file in the target folder
   $files = Get-ChildItem -Path .\Files
+   #For file, calculate the hash, and write to baseline.txt
+ foreach ($f in $files) {
+  $hash = Calculate-File-Hash $f.FullName
+  "$($hash.Path) | $($hash.Hash)" | Out-File -FilePath .\baseline.txt -Append
+    }
+}
+
+elseif ($response -eq "B".ToUpper()) {
+
+   $fileHashDictionary = @{}
+
+ #Load file|hash from baseline.txt and store them in a dictionary.
+ $filePathsAndHashes = Get-Content -Path .\baseline.txt
+ 
+ foreach ($f in $filePathsAndHashes) {
+   $fileHashDictionary.add($f.Split("|")[0],$f.Split("|")[1])
+ } }
